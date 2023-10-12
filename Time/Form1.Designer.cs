bfe0f93b35e49ec8cce1b2edad6d925f0fd2c75a
BraenAndDany <144007672+BraenAndDany@users.noreply.github.com>
2023-10-12 22:44:56 +0700

    Добавьте файлы проекта.

---
+++ Time/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Time = new System.Windows.Forms.Label();
+            this.List = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.Time.Location = new System.Drawing.Point(13, 6);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(121, 44);
+            this.Time.TabIndex = 2;
+            this.Time.Text = "label1";
+            // 
+            // List
+            // 
+            this.List.Location = new System.Drawing.Point(510, 12);
+            this.List.Multiline = true;
+            this.List.Name = "List";
+            this.List.ReadOnly = true;
+            this.List.Size = new System.Drawing.Size(263, 428);
+            this.List.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 452);
+            this.Controls.Add(this.List);
+            this.Controls.Add(this.Time);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.size);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.click);
+            this.Validated += new System.EventHandler(this.val);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.TextBox List;
+    }
+}
+
---
+++ Time/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Time
+{
+    public partial class Form1 : Form
+    {
+        string copy;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            string dirName = "C:\\";
+            // если папка существует
+            if (Directory.Exists(dirName))
+            {
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    List.Text = (s);
+                }
+               
+            }
+        }
+        async Task time()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Task.Delay(1000);
+                Time.Text = DateTime.Now.ToLongTimeString();
+                //string dirName = "C:\\";
+                //// если папка существует
+                //if (Directory.Exists(dirName))
+                //{
+                //    string[] dirs = Directory.GetDirectories(dirName);
+                //    foreach (string s in dirs)
+                //    {
+                //        List.Text = (s);
+                //    }
+                    
+                //}
+            }
+            
+        }
+        async private void click(object sender, MouseEventArgs e)
+        {
+            await time();
+        }
+
+        private void val(object sender, EventArgs e)
+        {
+
+        }
+
+        async private void size(object sender, EventArgs e)
+        {
+            
+        }
+        //async Task load()
+        //{
+        //    await Task.Delay(3000);     // имитация продолжительной работы
+        //                           // или так
+        //                                //await Task.Delay(TimeSpan.FromMilliseconds(3000));
+        //}
+    }
+}
