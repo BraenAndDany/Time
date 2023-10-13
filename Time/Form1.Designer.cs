229f041d4bfbc1cb9be59cb914bcb4e4034cd4cf
BraenAndDany <144007672+BraenAndDany@users.noreply.github.com>
2023-10-13 16:51:28 +0700

    add

--- Time/Form1.Designer.cs
+++ Time/Form1.Designer.cs
@@ -30,7 +30,8 @@
         {
             this.Time = new System.Windows.Forms.Label();
             this.Btn = new System.Windows.Forms.Button();
-            this.List = new System.Windows.Forms.Label();
+            this.Field = new System.Windows.Forms.TextBox();
+            this.Btn2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Time
@@ -53,25 +54,36 @@
             this.Btn.UseVisualStyleBackColor = true;
             this.Btn.Click += new System.EventHandler(this.Btn_Click);
             // 
-            // List
+            // Field
             // 
-            this.List.AutoSize = true;
-            this.List.Location = new System.Drawing.Point(485, 9);
-            this.List.Name = "List";
-            this.List.Size = new System.Drawing.Size(35, 13);
-            this.List.TabIndex = 5;
-            this.List.Text = "label1";
+            this.Field.Location = new System.Drawing.Point(469, 6);
+            this.Field.Multiline = true;
+            this.Field.Name = "Field";
+            this.Field.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Field.Size = new System.Drawing.Size(300, 445);
+            this.Field.TabIndex = 5;
+            this.Field.TextChanged += new System.EventHandler(this.Field_TextChanged);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Location = new System.Drawing.Point(141, 111);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(125, 39);
+            this.Btn2.TabIndex = 6;
+            this.Btn2.Text = "button1";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 452);
-            this.Controls.Add(this.List);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Field);
             this.Controls.Add(this.Btn);
             this.Controls.Add(this.Time);
-            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -86,7 +98,8 @@
         #endregion
         private System.Windows.Forms.Label Time;
         private System.Windows.Forms.Button Btn;
-        private System.Windows.Forms.Label List;
+        private System.Windows.Forms.TextBox Field;
+        private System.Windows.Forms.Button Btn2;
     }
 }
 
--- Time/Form1.cs
+++ Time/Form1.cs
@@ -14,24 +14,20 @@ namespace Time
 {
     public partial class Form1 : Form
     {
-        string copy;
-        bool stopper = false;
-        string dirName = "C:\\";
+        public string copy;
+
+        public bool stopper = false;
+        public string dirname = "C:\\";
+        public int score = 0;
+        public int scoreLine = 0;
+        public string[] file = new string[100];
+        public string[] dir = new string[100];
+        public int g =0;
         public Form1()
         {
             
             InitializeComponent();
         }
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         async void Form1_Load(object sender, EventArgs e)
         {
           
@@ -40,17 +36,62 @@ namespace Time
         {    
             await Task.Delay(1000);
             return DateTime.Now.ToLongTimeString();
-               if (Directory.Exists(dirName))
+            
+        }
+        //Проверка не появился ли новый файл или папка
+        public void Filelocal()
+        {
+                if (Directory.Exists(dirname))
                 {
-                    string[] dirs = Directory.GetDirectories(dirName);
+                    string[] dirs = Directory.GetDirectories(dirname);
                     foreach (string s in dirs)
                     {
-                        List.Text = (s)+"\n";
+                        if (dirs.Length < dir.Length)
+                        {
+                            Array.Resize(ref dirs, dir.Length);
+                            Field.Text += s + Environment.NewLine;
+                        }
+                        else if(dirs.Length > dir.Length)
+                        {
+                            Array.Resize(ref dir, dirs.Length);
+                            Field.Text += s + Environment.NewLine;
+                        }
+                        else if (s == dir[g])
+                        {
+                            Field.Text += s + Environment.NewLine;
+                        }
+                        
+                        
                     }
-
+                    
+                    score++;
                 }
-        }
+                g = 0;
+                if (Directory.Exists(dirname))
+                {
+                    string[] dirs = Directory.GetFiles(dirname);
+                    foreach (string s in dirs)
+                    {
+                        if(dirs.Length < file.Length)
+                        {
+                            Array.Resize(ref dirs, file.Length);
+                            Field.Text += s + Environment.NewLine;
+                        }
+                        else if (dirs.Length > file.Length)
+                        {
+                            Array.Resize(ref file, dirs.Length);
+                            Field.Text += s + Environment.NewLine;
+                        }
+                        else if (s == file[g])
+                        {
+                            Field.Text += s + Environment.NewLine;
+                        }
 
+                    }
+                }
+                g = 0;
+        }
+        //
         private void val(object sender, EventArgs e)
         {
 
@@ -63,21 +104,110 @@ namespace Time
 
         private async void Btn_Click(object sender, EventArgs e)
         {
+
             stopper = false;
             while (stopper==false)
-            {
+            {   
                 var time = await timelocal();
                 Time.Text = time.ToString();
-                
-                // если папка существует
-                
+
+
+            }
+            
+            
+            
+        }
+        //Забиваем массивы с папками и файлами
+        public void ListDir()
+        {
+            if (Directory.Exists(dirname))
+            {
+                string[] dirs = Directory.GetDirectories(dirname);
+                Array.Resize(ref dir, dirs.Length);
+                foreach (string s in dirs)
+                {
+                    Field.Text += s + Environment.NewLine;
+                    dir[g] = s;
+                    g++;
+                }
+                score++;
+            }
+            g = 0;
+        }
+        public void ListFiles()
+        {
+
+
+            if (Directory.Exists(dirname))
+            {
+                string[] dirs = Directory.GetFiles(dirname);
+                Array.Resize(ref file, dirs.Length);
+                foreach (string s in dirs)
+                {
+                    Field.Text += s + Environment.NewLine;
+                    file[g] = s;
+                    g++;
+
+                }
+            }
+            g = 0;
+            
+        }
+        //
+        private void Field_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        async public void Search()
+        {
+            ListDir();
+            ListFiles();
+            stopper = false;
+            while (stopper == false)
+            {
+                string[] dirs = Directory.GetDirectories(dirname);
+                string[] files = Directory.GetFiles(dirname);
+                if (dir.Length!=dirs.Length||file.Length!=files.Length)
+                {
+                    Field.Clear();
+                    ListDir();
+                    ListFiles();
+                }
+                foreach (string s in dirs)
+                {
+                    if (dir[g] != s)
+                    {
+                        Field.Clear();
+                        g=0;
+                        ListDir();
+                        ListFiles();
+                        break;
+                    }
+                    g++;
+
+                }
+                g=0;
+                foreach (string s in files)
+                {
+                    if (file[g] != s)
+                    {
+                        Field.Clear();
+                        g = 0;
+                        ListDir();
+                        ListFiles();
+                        break;
+                    }
+                    g++;
+
+                }
+                g = 0;
+                await Task.Delay(1);
             }
         }
-        //async Task load()
-        //{
-        //    await Task.Delay(3000);     // имитация продолжительной работы
-        //                           // или так
-        //                                //await Task.Delay(TimeSpan.FromMilliseconds(3000));
-        //}
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
     }
 }
